ce96318552745c36a571fdb34f2dd7ca47f12199
gaosung44 <gaosung44@gmail.com>
2026-09-21 19:46:34 +0900

    Delete flame directory

--- flame/script.js
+++
@@ -1,6 +0,0 @@
-const train = document.getElementById("img");
-train.addEventListener("click",()=>{
-    let isResult = confirm("この電車は好きですか?");
-    if (isResult) alert("ありがとう");
-    else alert("は?");
-});
